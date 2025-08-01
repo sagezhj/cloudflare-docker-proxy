50426b7f5e7111f9addbb22621f583f37a358e66
sagezhj <zhjah@163.com>
2025-08-01 13:05:26 +0800

    zhjah.top

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.zhjah.top": dockerHub,
+  "quay.zhjah.top": "https://quay.io",
+  "gcr.zhjah.top": "https://gcr.io",
+  "k8s-gcr.zhjah.top": "https://k8s.gcr.io",
+  "k8s.zhjah.top": "https://registry.k8s.io",
+  "ghcr.zhjah.top": "https://ghcr.io",
+  "cloudsmith.zhjah.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.zhjah.top": dockerHub,
 };
 
 function routeByHosts(host) {
